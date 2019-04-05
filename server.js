3b056fe0e343ba640fe24877dd361d6273cbfd82
Silva <silva@RALG-MacBookPro.local>
2019-04-05 13:50:49 -0600

    Second commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require("express");
+
+var app = express();
+
+// Set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 8889,
+  user: "root",
+  password: "root",
+  database: "friendfinder_db"
+});
